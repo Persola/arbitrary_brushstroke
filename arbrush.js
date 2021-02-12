278584361b1d311fc8ca623fd8bee83aba70212c
Luke Persola <lukepersola@gmail.com>
2021-02-11 17:20:58 -0800

    add brushtip indicator when pointer over canvas w/ adjustable size

--- arbrush.js
+++ arbrush.js
@@ -4,6 +4,10 @@ const $$ = document.querySelectorAll.bind(document);
 const VIEW_WIDTH = 600;
 const VIEW_HEIGHT = 600;
 
+const settings = {
+  brushtipSize: 10
+};
+
 // KICKOFF
 
 // should I be using void before putImageData?
@@ -61,9 +65,33 @@ const mouseInCanvas = (evt, rect) => {
   return false;
 };
 
+const updateBrushtipIndicatorSize = (diameter, pointerX, pointerY) => {
+  $('#brushtipIndicator').style.width = `${diameter}px`;
+  $('#brushtipIndicator').style.height = `${diameter}px`;
+  $('#brushtipIndicator').style.left = `${pointerX - (diameter / 2)}px`;
+  $('#brushtipIndicator').style.top = `${pointerY - (diameter / 2)}px`;
+  $('#brushtipIndicator').style['border-radius'] = `${(diameter / 2)}px`;
+}
+
+const updateBrushtipIndicatorPosition = (mouseInCanvasNow, pointerX, pointerY) => {
+  if (mouseInCanvasNow) {
+    const diameter = Number(
+      window.getComputedStyle($('#brushtipIndicator')).width.slice(0, -2)
+    );
+    $('#brushtipIndicator').style.display = 'block';
+    $('#brushtipIndicator').style.left = `${pointerX - (diameter / 2)}px`;
+    $('#brushtipIndicator').style.top = `${pointerY - (diameter / 2)}px`;
+    $('#brushtipIndicator').style['border-radius'] = `${(diameter / 2)}px`;
+  } else {
+    $('#brushtipIndicator').style.display = 'none';
+  }
+};
+
 const handlePointerMove = (evt) => {
   const rect = $('#view').getBoundingClientRect();
-  if (mouseInCanvas(evt, rect)) {
+  const mouseInCanvasNow = mouseInCanvas(evt, rect);
+  updateBrushtipIndicatorPosition(mouseInCanvasNow, evt.clientX, evt.clientY);
+  if (mouseInCanvasNow) {
     handleCanvasPointerMove(
       evt.clientX - rect.left,
       evt.clientY - rect.top
@@ -73,11 +101,15 @@ const handlePointerMove = (evt) => {
   }
 };
 
-window.onload = (e) => {
+window.onload = () => {
   const cantext = $('#view').getContext('2d');
   // $('#view').addEventListener("pointerdown", handleStart, false);
   // $('#view').addEventListener("pointerup", handleEnd, false);
   // $('#view').addEventListener("pointercancel", handleCancel, false);
   $('body').addEventListener('pointermove', handlePointerMove, true);
+  $('#brushtipSize').addEventListener('change', (evt) => {
+    settings.brushtipSize = $('#brushtipSize').value;
+    updateBrushtipIndicatorSize(settings.brushtipSize, evt.clientX, evt.clientY);
+  }, false);
   initializeView();
 };
